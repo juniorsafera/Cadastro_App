de943b9414c2fcfcce502026c22eed764f5824c8
Junior Safera <juniorsafera.dev@gmail.com>
2021-06-29 18:51:22 -0300

    update provider

--- cadastro_app/lib/main.dart
+++ cadastro_app/lib/main.dart
@@ -1,6 +1,4 @@
  
- 
-import 'package:cadastro_app/provider/user.dart';
 import 'package:cadastro_app/views/user_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
